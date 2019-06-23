6350ce2e6ad1405bda856458485825e85e76b7e4
Audra Tallman <audi.tallman@gmail.com>
2019-06-23 22:29:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ function  fuzzyMatch (list, partialName) {
   });
 }
 
-function matchName (list,name) {
-  return list.filter(function(driverName){
+function matchName (list, name) {
+  return list.filter(function(driver){
     return driver.name.toLowerCase() ===  name.toLowerCase();
   });
 }
